9974dc0310b249132aa99bf95bd96aee8e45bd52
Alessandro Ramos <toprime13@gmail.com>
2022-12-29 14:13:13 -0300

    Add PWA

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { ColorModeScript } from "@chakra-ui/react";
+import { SITE } from "configs/site";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import { theme } from "theme";
 
@@ -7,6 +8,36 @@ export default class MyDocument extends Document {
 		return (
 			<Html lang="pt-br">
 				<Head>
+					<meta charSet="utf-8" />
+					<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+					<meta name="application-name" content={SITE.name} />
+					<meta name="description" content="Description" />
+					<meta name="keywords" content="Keywords" />
+					<meta name="apple-mobile-web-app-capable" content="yes" />
+					<meta
+						name="apple-mobile-web-app-status-bar-style"
+						content="default"
+					/>
+					<meta name="apple-mobile-web-app-title" content={SITE.name} />
+					<meta name="format-detection" content="telephone=no" />
+					<meta name="mobile-web-app-capable" content="yes" />
+					<meta name="theme-color" content="#0070F3" />
+
+					<link rel="manifest" href="/manifest.json" />
+					<link rel="shortcut icon" href="/favicon.ico" />
+					<link
+						href="/icons/favicon-16x16.png"
+						rel="icon"
+						type="image/png"
+						sizes="16x16"
+					/>
+					<link
+						href="/icons/favicon-32x32.png"
+						rel="icon"
+						type="image/png"
+						sizes="32x32"
+					/>
+					<link rel="apple-touch-icon" href="/apple-icon.png" />
 					<link
 						href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
 						rel="stylesheet"
