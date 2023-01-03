aa583c475e39b099139ca7d1b71be5145143c153
Alessandro H. Ramos <70819804+aleessandrohr@users.noreply.github.com>
2023-01-03 10:57:53 -0300

    Remove Next Export (#2)

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -1,12 +1,4 @@
-import type { ImageProps, ImageLoader } from "next/image";
+import type { ImageProps } from "next/image";
 import ImageNext from "next/image";
 
-const DEFAULT_QUALITY = 75;
-
-const loader: ImageLoader = ({ src, width, quality }) => {
-	return `${src}?w=${width}&q=${quality || DEFAULT_QUALITY}`;
-};
-
-export const Image: FC<ImageProps> = props => (
-	<ImageNext loader={loader} {...props} />
-);
+export const Image: FC<ImageProps> = props => <ImageNext {...props} />;
