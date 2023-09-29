3118fe1493ca0832c299b5c594023ce0a47d1ff3
Thiago Spart <user000.000resu@gmail.com>
2023-09-28 21:12:50 -0300

    feat: add auth route structure

---
+++ src/components/Input/types.ts
@@ -0,0 +1,11 @@
+import type { InputHTMLAttributes } from "react"
+import { FieldError, FieldErrorsImpl, Merge } from "react-hook-form";
+
+export interface InputProps extends InputHTMLAttributes<HTMLInputElement>{
+	errorMessage?: FieldError | Partial<{
+    type: string | number;
+    message: string;
+}> | Merge<FieldError, FieldErrorsImpl<any>>;
+	labelMessage?: string;
+	isFullWidth?: boolean;
+}
