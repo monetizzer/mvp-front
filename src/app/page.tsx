ff674c08cb032381ac0cfcbc415135926588681f
Henrique Leite <contact.henriqueleite42@gmail.com>
2023-10-15 15:27:51 -0300

    Add Create Store Page (#12)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,24 @@ import Link from "next/link";
 const Home = () => {
 	return (
 		<main className="min-h-[100dvh] w-full flex justify-center">
-			<section className="max-w-7xl container-padding w-full flex items-center justify-center">
-				<Link className="text-white btn btn-secondary rounded-xl" href="/login">
+			<section className="max-w-7xl container-padding w-full flex flex-col items-center justify-center gap-4">
+				<Link
+					className="w-full text-white btn btn-secondary rounded-xl"
+					href="/login"
+				>
 					Entrar ou criar conta
+				</Link>
+				<Link
+					className="w-full text-white btn btn-secondary rounded-xl"
+					href="/documentos"
+				>
+					Enviar documentos
+				</Link>
+				<Link
+					className="w-full text-white btn btn-secondary rounded-xl"
+					href="/criar-loja"
+				>
+					Criar loja
 				</Link>
 			</section>
 		</main>
