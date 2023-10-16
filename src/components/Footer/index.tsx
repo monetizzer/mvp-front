7e01510c2dfffdd8172568c9065c6352167865df
Henrique Leite <contact.henriqueleite42@gmail.com>
2023-10-16 15:40:29 -0300

    Add Footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export const Footer = () => {
+	return (
+		<div className="flex flex-col text-center bg-neutral text-neutral-content container-padding">
+			<span className="text-4xl mb-2">🔞</span>
+			<span>Todas as modelos neste site sao maiores de 18 anos.</span>
+		</div>
+	);
+};
