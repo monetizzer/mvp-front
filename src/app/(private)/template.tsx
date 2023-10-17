e3d07d9f0f33312a75ec707901d0d25cdfdd9a6b
Alessandro Ramos <toprime13@gmail.com>
2023-10-17 20:57:19 -0300

    Add Loading

--- src/app/(private)/template.tsx
+++ src/app/(private)/template.tsx
@@ -1,21 +1,20 @@
 "use client";
 
 import { useAuth } from "hooks/useAuth";
-import { useRouter } from "next/navigation";
+import { redirect, RedirectType } from "next/navigation";
 import { ReactNode } from "react";
 
 interface Props {
 	children: ReactNode;
 }
 
-const PublicLayout: FC<Props> = ({ children }) => {
-	const router = useRouter();
+const PrivateLayout: FC<Props> = ({ children }) => {
 	const auth = useAuth();
 	const { user } = auth;
 
-	if (user) router.replace("/user");
+	if (user === null) redirect("/login", RedirectType.replace);
 
 	return <>{children}</>;
 };
 
-export default PublicLayout;
+export default PrivateLayout;
