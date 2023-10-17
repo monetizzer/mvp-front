393762a498021deae4e7d6086e9f0ae2e46b8816
Alessandro Ramos <toprime13@gmail.com>
2023-10-17 20:57:19 -0300

    Add Loading

--- src/app/(public)/template.tsx
+++ src/app/(public)/template.tsx
@@ -1,21 +1,20 @@
 "use client";
 
 import { useAuth } from "hooks/useAuth";
-import { useRouter } from "next/navigation";
+import { redirect, RedirectType } from "next/navigation";
 import { ReactNode } from "react";
 
 interface Props {
 	children: ReactNode;
 }
 
-const PrivateLayout: FC<Props> = ({ children }) => {
-	const router = useRouter();
+const PublicLayout: FC<Props> = ({ children }) => {
 	const auth = useAuth();
 	const { user } = auth;
 
-	if (user === null) router.replace("/login");
+	if (user) redirect("/user", RedirectType.replace);
 
 	return <>{children}</>;
 };
 
-export default PrivateLayout;
+export default PublicLayout;
--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -18,7 +18,7 @@ const API_URL = process.env["NEXT_PUBLIC_API_URL"];
 export const AuthContext = createContext({} as IAuthContext);
 
 export const AuthProvider: FC<Props> = ({ children }) => {
-	const [user, setUser] = useState<User | undefined>(undefined);
+	const [user, setUser] = useState<User | null | undefined>(undefined);
 
 	useEffect(() => {
 		const userFetch = async (token: string) => {
@@ -43,13 +43,30 @@ export const AuthProvider: FC<Props> = ({ children }) => {
 			return;
 		}
 
-		setUser(undefined);
+		setUser(null);
 	}, []);
 
 	const logout = () => {
 		deleteCookie("token");
-		setUser(undefined);
+		setUser(null);
 	};
+
+	if (user === undefined) {
+		return (
+			<main className="flex justify-center w-full min-h-[100dvh]">
+				<section className="flex items-center justify-center w-full max-w-7xl container-padding">
+					<div className="card w-full max-w-md flex-shrink-0 xs:bg-base-200 xs:shadow-md container-padding">
+						<header className="card-body flex items-center justify-center w-full mb-8">
+							<h2 className="card-title text-3xl text-center">Las Musas</h2>
+						</header>
+						<div className="flex items-center justify-center flex-col">
+							<progress className="progress w-56"></progress>
+						</div>
+					</div>
+				</section>
+			</main>
+		);
+	}
 
 	return (
 		<AuthContext.Provider value={{ user, logout }}>
