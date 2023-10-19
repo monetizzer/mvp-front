ceb21e50338becce82fbc86b8c7eb828bbc61734
Henrique Leite <contact.henriqueleite42@gmail.com>
2023-10-19 10:26:14 -0300

    Add ProductCard (#25)

--- src/app/(public)/conteudo/[productId]/page.tsx
+++ src/app/(public)/conteudo/[productId]/page.tsx
@@ -2,14 +2,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 import { products, store } from "assets/data";
+import { ProductCard } from "components/ProductCard";
+import { ProductMediasCount } from "components/ProductMediasCount";
 import { ProductTypeBadge } from "components/ProductTypeBadge";
 import Link from "next/link";
-import {
-	BsCameraVideoFill,
-	BsImageFill,
-	BsPencilFill,
-	BsVolumeUpFill,
-} from "react-icons/bs";
+import { BsPencilFill } from "react-icons/bs";
 import { IoMdArrowRoundBack } from "react-icons/io";
 import { MdVerified } from "react-icons/md";
 import { PiShoppingCartSimpleFill } from "react-icons/pi";
@@ -45,47 +42,7 @@ const Content = ({ params }: any) => {
 					<h1 className="text-2xl font-bold col-span-3">{product.name}</h1>
 					<ProductTypeBadge type={product.type} />
 				</div>
-				<div className="flex gap-2 w-full">
-					{product.mediasCount?.VIDEO && (
-						<div
-							className="flex badge-video border tooltip"
-							data-tip={`${product.mediasCount?.VIDEO} videos inclusos`}
-						>
-							<div className="h-full flex items-center rounded-tl-lg rounded-bl-lg px-2">
-								<BsCameraVideoFill />
-							</div>
-							<div className="w-1/2 h-full font-bold flex items-center rounded-tr-lg rounded-br-lg px-2">
-								{product.mediasCount?.VIDEO}
-							</div>
-						</div>
-					)}
-					{product.mediasCount?.IMAGE && (
-						<div
-							className="flex badge-image border tooltip"
-							data-tip={`${product.mediasCount?.IMAGE} imagens inclusos`}
-						>
-							<div className="h-full flex items-center rounded-tl-lg rounded-bl-lg px-2">
-								<BsImageFill />
-							</div>
-							<div className="w-1/2 h-full font-bold flex items-center rounded-tr-lg rounded-br-lg px-2">
-								{product.mediasCount?.IMAGE}
-							</div>
-						</div>
-					)}
-					{product.mediasCount?.AUDIO && (
-						<div
-							className="flex badge-audio border tooltip"
-							data-tip={`${product.mediasCount?.AUDIO} audios inclusos`}
-						>
-							<div className="h-full flex items-center rounded-tl-lg rounded-bl-lg px-2">
-								<BsVolumeUpFill />
-							</div>
-							<div className="w-1/2 h-full font-bold flex items-center rounded-tr-lg rounded-br-lg px-2">
-								{product.mediasCount?.AUDIO}
-							</div>
-						</div>
-					)}
-				</div>
+				<ProductMediasCount mediasCount={product.mediasCount} />
 				<p className="text-sm text-center mb-2 whitespace-pre-line">
 					{product.description}
 				</p>
@@ -122,71 +79,7 @@ const Content = ({ params }: any) => {
 
 				<section className="flex flex-col gap-4">
 					{products.map((p) => {
-						return (
-							<div key={p.productId} className="flex flex-col gap-2">
-								<div className="carousel rounded-md">
-									{p.previewImagesUrls.map((url, idx) => (
-										<Link
-											key={url}
-											className="carousel-item w-full"
-											href={`/conteudo/${p.productId}`}
-										>
-											<img src={url} alt={p.name + idx} className="w-full" />
-										</Link>
-									))}
-								</div>
-								<div className="flex justify-between gap-2 w-full">
-									<span className="font-bold col-span-3">{p.name}</span>
-									<ProductTypeBadge type={p.type} />
-								</div>
-								<div className="flex gap-2 w-full">
-									{p.mediasCount?.VIDEO && (
-										<div
-											className="flex badge-video border tooltip"
-											data-tip={`${p.mediasCount?.VIDEO} videos inclusos`}
-										>
-											<div className="h-full flex items-center rounded-tl-lg rounded-bl-lg px-2">
-												<BsCameraVideoFill />
-											</div>
-											<div className="w-1/2 h-full font-bold flex items-center rounded-tr-lg rounded-br-lg px-2">
-												{p.mediasCount?.VIDEO}
-											</div>
-										</div>
-									)}
-									{p.mediasCount?.IMAGE && (
-										<div
-											className="flex badge-image border tooltip"
-											data-tip={`${p.mediasCount?.IMAGE} imagens inclusos`}
-										>
-											<div className="h-full flex items-center rounded-tl-lg rounded-bl-lg px-2">
-												<BsImageFill />
-											</div>
-											<div className="w-1/2 h-full font-bold flex items-center rounded-tr-lg rounded-br-lg px-2">
-												{p.mediasCount?.IMAGE}
-											</div>
-										</div>
-									)}
-									{p.mediasCount?.AUDIO && (
-										<div
-											className="flex badge-audio border tooltip"
-											data-tip={`${p.mediasCount?.AUDIO} audios inclusos`}
-										>
-											<div className="h-full flex items-center rounded-tl-lg rounded-bl-lg px-2">
-												<BsVolumeUpFill />
-											</div>
-											<div className="w-1/2 h-full font-bold flex items-center rounded-tr-lg rounded-br-lg px-2">
-												{p.mediasCount?.AUDIO}
-											</div>
-										</div>
-									)}
-								</div>
-								<button className="btn btn-secondary">
-									<PiShoppingCartSimpleFill />
-									Comprar por {formatMoney(p.price)}
-								</button>
-								<span className="text-sm text-center">Acesso imediato!</span>
-							</div>
-						);
+						return <ProductCard key={p.productId} product={p} />;
 					})}
 				</section>
 			</section>
